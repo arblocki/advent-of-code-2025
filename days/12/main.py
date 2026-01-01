7569e48e3bb68df520c35e01f241e02a5dedb43b
Drew Blocki <andrewrblocki@gmail.com>
2026-01-01 08:36:34 -0700

    day 12

--- days/12/main.py
+++ days/12/main.py
@@ -9,3 +9,29 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspa
 from utils import get_input
 
 input = get_input(__file__)
+sections = input.split('\n\n')
+
+multipler = 7
+
+totalValid = 0
+for line in sections[6].splitlines():
+    parts = line.split(' ')
+
+    [wStr, hStr] = parts[0][:-1].split('x')
+    w = int(wStr)
+    h = int(hStr)
+    size = w * h
+    
+    partSum = 0
+    for part in parts[1:]:
+        partCount = int(part)
+        partSum += partCount
+    
+    if partSum * multipler < size:
+        print(f'VALID: \t\t{line}')
+        totalValid += 1
+    else:
+        print(f'INVALID: \t{line}')
+
+print()
+print(totalValid)
